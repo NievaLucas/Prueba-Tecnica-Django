be1f688ad77995ebc045aeaf7f666224199cb48e
Nieva Lucas <nievalucas60@gmail.com>
2025-02-12 01:03:39 -0300

    Cambio de funciones a class based views

--- app/views.py
+++ app/views.py
@@ -1,32 +1,54 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse
-from .forms import formTransaccion
+from django.views import View
+from .forms import FormTransaccion
 from .models import Transaccion
 
 # Create your views here.
-
-def accounts(request) :
-
-    return render(request, 'accounts.html')
-
-def accountsTransaccion(_request) :
-        
+ 
+def jsonAccount(self, _request) :
     historialTransacciones = list(Transaccion.objects.values())
-    date = {'historialTransacciones' : historialTransacciones}
-    return JsonResponse(date)
+    return JsonResponse({'historialTransacciones' : historialTransacciones})
 
-def sendTransaccion(request) :
+# Vista a DataTables
+class Account(View) :
+    def get(self, request) :
+        return render(request, 'accounts.html')
 
-    if request.method == 'POST' :
-        form = formTransaccion(request.POST)
-        if form.is_valid() :
+template_name = "sendTransaccion.html"
 
-            # Guardamos en la Base de Datos
-            form.save()
+# Vista y funcionamiento de subida de Transacciones
+class SendTransaccion(View) :
 
-            return redirect('accounts')
-    else :
-        form = formTransaccion()
-    return render(request, 'sendTransaccion.html', {'form' : form})
+    formClass = FormTransaccion()
     
+    def get(self, request, *args, **kwargs) : 
+        return render(request, template_name, {'form' : self.formClass})
+    
+    def post(self, request, *args, **kwargs) :
+        form = FormTransaccion(request.POST)
+
+        if form.is_valid() :
+            form.save()
+            return redirect('accounts')
+        
+        return render(request, template_name, {'form' : self.formClass})
+    
+# Vista al UPDATE de las transferencias
+class UpdateTransaccion(View) :
+
+    def get(self, request, id, *args, **kwargs) :
+        value = Transaccion.objects.get(pk = id)
+        form = FormTransaccion(instance = value)
+        return render(request, template_name, {'form' : form})
+
+    def post(self, request, id, *arg, **kwargs) :
+       
+        instance = Transaccion.objects.get(pk = id)
+        form = FormTransaccion(request.POST, instance = instance)
+
+        if form.is_valid() :
+            form.save()
+            return redirect('accounts')
+        
+        return render(request, template_name, {'form' : form})
