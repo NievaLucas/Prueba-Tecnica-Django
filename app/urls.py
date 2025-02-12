7da63f2536d2da33a8b491d1a7a2e529fe12fc7a
Nieva Lucas <nievalucas60@gmail.com>
2025-02-12 01:02:07 -0300

    Arreglo de lista y cambio de rutas en urlpatterns

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
-    path("accounts/", views.accounts, name = 'accounts'),
-    path("accountsJSON/", views.accountsTransaccion, name = 'accountsTransaccion'),
-    path("sendAccounts/", views.sendTransaccion, name = 'sendTransaccion')
-}
+urlpatterns = [
+    path("accounts/", views.Account.as_view(), name = 'accounts'),
+    path("accountsJSON/", views.jsonAccount, name = 'accountsTransaccion'),
+    path("accountsSend/", views.SendTransaccion.as_view(), name = 'accountSend'),
+    path("accountsUpdate/<int:id>/", views.UpdateTransaccion.as_view(), name = "accountUpdate")
+]
