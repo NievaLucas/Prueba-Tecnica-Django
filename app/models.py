e1d5ebe19b7a1c06666c388ec3aa5f1fe3b7ca1b
Nieva Lucas <nievalucas60@gmail.com>
2025-02-12 00:33:37 -0300

    Cambio campo 'descripcion' a blank=true

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Transaccion(models.Model) :
     numeroDeCuenta = models.IntegerField(unique = True, blank = False)
     numeroComprobante = models.CharField(max_length = 20, blank = False)
     tipoDeComprobante = models.CharField(max_length = 20, blank = False)
-    descripcion = models.CharField(max_length = 1000)
+    descripcion = models.CharField(max_length = 1000, blank = True)
     debito = models.DecimalField(max_digits = 10, decimal_places = 2, blank = False)
     credito = models.DecimalField(max_digits = 10, decimal_places = 2, blank = False)
     
